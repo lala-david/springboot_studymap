112431ed01e856bd2a725cf365060ccedc7d43db
lala-david <wnswns1946@naver.com>
2023-01-16 19:26:56 +0900

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Main class
 public class Application {
     public static void main(String[] args){
-        SpringApplication.run(Application.class, args);
+          SpringApplication.run(Application.class, args);
     }
 }
